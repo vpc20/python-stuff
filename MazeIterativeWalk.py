c880ea9de32c538d6eec81fdd6855b3030db63c2
vpc <cuevasvp@gmail.com>
2020-06-18 12:08:37 +0800

    refactor code

--- MazeIterativeWalk.py
+++ MazeIterativeWalk.py
@@ -50,84 +50,103 @@ def search(maze, row, col):
 #     return maze
 
 
-def maze_iter(maze, x, y):
-    stack = [(x, y)]
-    maze[x][y] = VISITED
-    xy_delta = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+def maze_iter(maze, row, col):
+    stack = [(row, col)]
+    maze[row][col] = VISITED
     while stack:
-        for xd, yd in xy_delta:
-            if -1 < x + xd < len(maze) and -1 < y + yd < len(maze):
-                if maze[x + xd][y + yd] == TARGET_CELL:
+        for r, c in [(row + 1, col), (row, col + 1), (row - 1, col), (row, col - 1)]:
+            if -1 < r < len(maze) and -1 < c < len(maze):
+                if maze[r][c] == TARGET_CELL:
                     return maze
-                if maze[x + xd][y + yd] == EMPTY_CELL:
-                    stack.append((x, y))
-                    x = x + xd
-                    y = y + yd
-                    maze[x][y] = VISITED
+                if maze[r][c] == EMPTY_CELL:
+                    stack.append((row, col))
+                    maze[r][c] = VISITED
+                    row = r
+                    col = c
                     break
         else:
-            x, y = stack.pop()
-    return maze
+            row, col = stack.pop()
+    return False
 
-    # def check_maze(maze, row, col):
-    #     if maze[row][col] == EMPTY_CELL:
-    #         print 'Visiting cell', row, col
-    #         maze[row][col] = VISITED
-    #         stack.append((row, col))
-    #     elif maze[row][col] == TARGET_CELL:
-    #         print 'Target found at', row, col
-    #         return True
-    #     elif maze[row][col] == WALL:
-    #         print 'Wall encountered at', row, col
-    #     elif maze[row][col] == VISITED:
-    #         print 'Visited cell at', row, col
 
-    # def search(maze, row, col):
-    #     print 'Visiting cell', row, col
-    #     maze[row][col] = VISITED
-    #     stack.append((row, col))
-    #
-    #     while True:
-    #         # up
-    #         if row - 1 > -1 and maze[row - 1][col] != EMPTY_CELL and maze[row - 1][col] != TARGET_CELL:
-    #             row -= 1
-    #             if check_maze(maze, row, col):
-    #                 break
-    #         # right
-    #         elif col + 1 < len(maze) - 1 and maze[row][col + 1] != EMPTY_CELL and maze[row][col + 1] != TARGET_CELL:
-    #             col += 1
-    #             if check_maze(maze, row, col):
-    #                 break
-    #         # down
-    #         elif row + 1 < len(maze) - 1 and maze[row + 1][col] != EMPTY_CELL and maze[row + 1][col] != TARGET_CELL:
-    #             row += 1
-    #             if check_maze(maze, row, col):
-    #                 break
-    #         # left
-    #         elif col - 1 > -1 and maze[row][col - 1] != EMPTY_CELL and maze[row][col - 1] != TARGET_CELL:
-    #             col -= 1
-    #             if check_maze(maze, row, col):
-    #                 break
-    #         else:
-    #             x = stack.pop()
-    #             row = x[0]
-    #             col = x[1]
-    #
-    #
-    # def check_maze(maze, row, col):
-    #     if maze[row][col] == EMPTY_CELL:
-    #         print 'Visiting cell', row, col
-    #         maze[row][col] = VISITED
-    #         stack.append((row, col))
-    #     elif maze[row][col] == TARGET_CELL:
-    #         print 'Target found at', row, col
-    #         return True
-    #     elif maze[row][col] == WALL:
-    #         print 'Wall encountered at', row, col
-    #     elif maze[row][col] == VISITED:
-    #         print 'Visited cell at', row, col
+# def maze_iter(maze, x, y):
+#     stack = [(x, y)]
+#     maze[x][y] = VISITED
+#     xy_delta = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+#     while stack:
+#         for xd, yd in xy_delta:
+#             if -1 < x + xd < len(maze) and -1 < y + yd < len(maze):
+#                 if maze[x + xd][y + yd] == TARGET_CELL:
+#                     return maze
+#                 if maze[x + xd][y + yd] == EMPTY_CELL:
+#                     stack.append((x, y))
+#                     x = x + xd
+#                     y = y + yd
+#                     maze[x][y] = VISITED
+#                     break
+#         else:
+#             x, y = stack.pop()
+#     return maze
+#
+# def check_maze(maze, row, col):
+#     if maze[row][col] == EMPTY_CELL:
+#         print 'Visiting cell', row, col
+#         maze[row][col] = VISITED
+#         stack.append((row, col))
+#     elif maze[row][col] == TARGET_CELL:
+#         print 'Target found at', row, col
+#         return True
+#     elif maze[row][col] == WALL:
+#         print 'Wall encountered at', row, col
+#     elif maze[row][col] == VISITED:
+#         print 'Visited cell at', row, col
 
-    # pop from stack if there are no more paths available in any direction  # maze = [[0, 0, 0, 0, 0, 1],
+# def search(maze, row, col):
+#     print 'Visiting cell', row, col
+#     maze[row][col] = VISITED
+#     stack.append((row, col))
+#
+#     while True:
+#         # up
+#         if row - 1 > -1 and maze[row - 1][col] != EMPTY_CELL and maze[row - 1][col] != TARGET_CELL:
+#             row -= 1
+#             if check_maze(maze, row, col):
+#                 break
+#         # right
+#         elif col + 1 < len(maze) - 1 and maze[row][col + 1] != EMPTY_CELL and maze[row][col + 1] != TARGET_CELL:
+#             col += 1
+#             if check_maze(maze, row, col):
+#                 break
+#         # down
+#         elif row + 1 < len(maze) - 1 and maze[row + 1][col] != EMPTY_CELL and maze[row + 1][col] != TARGET_CELL:
+#             row += 1
+#             if check_maze(maze, row, col):
+#                 break
+#         # left
+#         elif col - 1 > -1 and maze[row][col - 1] != EMPTY_CELL and maze[row][col - 1] != TARGET_CELL:
+#             col -= 1
+#             if check_maze(maze, row, col):
+#                 break
+#         else:
+#             x = stack.pop()
+#             row = x[0]
+#             col = x[1]
+#
+#
+# def check_maze(maze, row, col):
+#     if maze[row][col] == EMPTY_CELL:
+#         print 'Visiting cell', row, col
+#         maze[row][col] = VISITED
+#         stack.append((row, col))
+#     elif maze[row][col] == TARGET_CELL:
+#         print 'Target found at', row, col
+#         return True
+#     elif maze[row][col] == WALL:
+#         print 'Wall encountered at', row, col
+#     elif maze[row][col] == VISITED:
+#         print 'Visited cell at', row, col
+
+# pop from stack if there are no more paths available in any direction  # maze = [[0, 0, 0, 0, 0, 1],
 
 
 # maze1 = [[0, 0, 0, 1, 0, 2],
@@ -137,15 +156,15 @@ def maze_iter(maze, x, y):
 #          [0, 1, 0, 1, 0, 1],
 #          [0, 1, 0, 0, 0, 1]]
 
-maze1 = [[0, 0, 0, 1, 0, 2],
-         [0, 1, 0, 1, 0, 1],
-         [0, 1, 0, 1, 0, 1],
-         [1, 1, 1, 1, 0, 1],
-         [0, 1, 0, 1, 0, 1],
-         [0, 0, 0, 0, 0, 1]]
-
-print(search(maze1, 5, 0))
-print(maze1)
+# maze1 = [[0, 0, 0, 1, 0, 2],
+#          [0, 1, 0, 1, 0, 1],
+#          [0, 1, 0, 1, 0, 1],
+#          [1, 1, 1, 1, 0, 1],
+#          [0, 1, 0, 1, 0, 1],
+#          [0, 0, 0, 0, 0, 1]]
+#
+# print(search(maze1, 5, 0))
+# print(maze1)
 # for e in maze1:
 #     print(e)
 
@@ -164,5 +183,5 @@ maze1 = [[0, 0, 0, 1, 0, 2],
          [0, 0, 0, 0, 0, 1]]
 
 print(maze_iter(maze1, 5, 0))
-# for e in maze1:
-#     print(e)
+for e in maze1:
+    print(e)
--- MazeRecursiveWalk.py
+++ MazeRecursiveWalk.py
@@ -77,23 +77,39 @@ def search(maze, row, col):
 #         [0, 1, 0, 0, 1, 0],
 #         [0, 1, 0, 0, 0, 2]]
 
-def maze_recur(maze, startx, starty):
-    def _maze_recur(x, y):
-        xy_delta = [(-1, 0), (0, 1), (1, 0), (0, -1)]
-        for xd, yd in xy_delta:
-            if -1 < x + xd < len(maze) and -1 < y + yd < len(maze):
-                if maze[x + xd][y + yd] == TARGET_CELL:
+# def maze_recur(maze, startx, starty):
+#     def _maze_recur(x, y):
+#         xy_delta = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+#         for xd, yd in xy_delta:
+#             if -1 < x + xd < len(maze) and -1 < y + yd < len(maze):
+#                 if maze[x + xd][y + yd] == TARGET_CELL:
+#                     return True
+#                 if maze[x + xd][y + yd] == EMPTY_CELL:
+#                     maze[x + xd][y + yd] = VISITED
+#                     if _maze_recur(x + xd, y + yd):
+#                         return True
+#                     maze[x + xd][y + yd] = EMPTY_CELL
+#         return False
+#
+#     _maze_recur(startx, starty)
+#     return maze
+
+
+def maze_recur(maze, start_row, start_col):
+    def dfs(row, col):
+        for r, c in [(row - 1, col), (row, col + 1), (row + 1, col), (row, col - 1)]:
+            if -1 < r < len(maze) and -1 < c < len(maze):
+                if maze[r][c] == TARGET_CELL:
                     return True
-                if maze[x + xd][y + yd] == EMPTY_CELL:
-                    maze[x + xd][y + yd] = VISITED
-                    if _maze_recur(x + xd, y + yd):
+                if maze[r][c] == EMPTY_CELL:
+                    maze[r][c] = VISITED
+                    if dfs(r, c):
                         return True
-                    maze[x + xd][y + yd] = EMPTY_CELL
+                    maze[r][c] = EMPTY_CELL
         return False
 
-    _maze_recur(startx, starty)
+    dfs(start_row, start_col)
     return maze
-
 
 # maze1 = [[1, 1, 0, 1, 0, 2],
 #         [0, 0, 0, 0, 1, 0],
@@ -113,3 +129,5 @@ maze1 = [[1, 1, 0, 1, 0, 2],
 #     print(e)
 
 print(maze_recur(maze1, 5, 0))
+for e in maze_recur(maze1, 5, 0):
+    print(e)
