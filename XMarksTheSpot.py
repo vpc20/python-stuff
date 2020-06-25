912d7aa2b97122739d6fe2e54012024e800582a9
vpc <cuevasvp@gmail.com>
2020-06-26 06:24:43 +0800

    refactor code

--- XMarksTheSpot.py
+++ XMarksTheSpot.py
@@ -14,90 +14,18 @@
 #           [0, 0, 1, 1, 0, 0],
 #           [0, 1, 0, 0, 1, 0],
 #           [1, 0, 0, 0, 0, 1]];
-# def x(n):
-#     arr1 = []
-#     for i in range(n):
-#         arr2 = []
-#         x = -99
-#         for j in range(n):
-#             if j == i:
-#                 arr2.append(1)
-#                 x = n - j - 1
-#             else:
-#                 if j == x:
-#                     arr2.append(1)
-#                 else:
-#                     arr2.append(0)
-#         print arr2
-
-# def x(n):
-#     arr1 = []
-#     for i in range(n):
-#         arr2 = []
-#         for j in range(n):
-#             if j == i or j == n - i - 1:
-#                 arr2.append(1)
-#             else:
-#                 arr2.append(0)
-#         arr1.append(arr2)
-#     return arr1
-
-# def x(n):
-#     arr = [1 if j == i or j == n - i - 1 else 0 for i in range(n) for j in range(n)]
-#     return [arr[i * n:i * n + n] for i in range(n)]
-
-# def x(n):
-#     return [[1 if j == i or j == n - i - 1 else 0 for i in range(n)] for j in range(n)]
-
-# def x(n):
-#     return [[int(j == i or j == n - i - 1) for i in range(n)] for j in range(n)]
-
-# def x(n):
-#     d = [[0] * n for i in range (n)]
-#     for i in range(n):
-#         d[i][i] = 1
-#         d[i][-i-1] = 1
-#     return d
-
-# def fill(i, j, n):
-#     if i == j or i + j == n - 1:
-#        return 1
-#     else:
-#        return 0
-#
-# def x(n):
-#     return [[fill(i, j, n) for j in range(n)] for i in range(n)]
-
-# def x(n):
-#     return [[int(x == y or x == n - y - 1) for x in xrange(n)] for y in xrange(n)]
 
 
 def x(n):
-    output = []
+    xarr = [[0] * n for _ in range(n)]
     for i in range(n):
-        inner_arr = []
-        for j in range(n):
-            if i == j or i == n - 1 - j:
-                inner_arr.append(1)
-            else:
-                inner_arr.append(0)
-        output.append(inner_arr)
-        print(inner_arr)
-    return output
+        xarr[i][i] = 1
+        xarr[i][n - 1 - i] = 1
+    return xarr
 
 
-# def x(n):
-#     return [[1 if i == j or i == n - j - 1 else 0 for i in range(n)] for j in range(n)]
-
-
-# print x(1)
-# print x(2)
-# print x(3)
-# print x(4)
-print(x(5))
-
-# [[1, 0, 0, 0, 1],
-#  [0, 1, 0, 1, 0],
-#  [0, 0, 1, 0, 0],
-#  [0, 1, 0, 1, 0],
-#  [1, 0, 0, 0, 1]]
+for i in range(6):
+    arr = x(i)
+    for e in arr:
+        print(e)
+    print('')
