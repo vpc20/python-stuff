8385ad95244a0f2e20ce6d7a492318115f54028d
vpc20 <cuevasvp@yahoo.com>
2024-12-23 07:18:16 +0800

    new

--- maze_dfs_single_path.py
+++ maze_dfs_single_path.py
@@ -14,28 +14,6 @@ def print_path_in_maze(maze, path):
     print()
 
 
-def maze_to_matrix(maze_str):
-    """
-    Converts a string representation of a maze into a 2D matrix.
-
-    Args:
-      maze_str: The string representation of the maze.
-
-    Returns:
-      A 2D list representing the maze.
-    """
-    rows = maze_str.count('\n') + 1
-    x = maze_str.split('\n')
-    cols = len(maze_str.split('\n')[0])
-    maze_matrix = []
-    for i in range(rows):
-        row = []
-        for j in range(cols):
-            row.append(maze_str[i * (cols + 1) + j])
-        maze_matrix.append(row)
-    return maze_matrix
-
-
 def find_start_end(maze_matrix):
     """
     Finds the starting and ending positions in the maze matrix.
@@ -61,12 +39,12 @@ def find_start_end(maze_matrix):
     return start, end
 
 
-def dfs(maze_matrix, start, end):
+def dfs(maze, start, end):
     """
     Performs Depth-First Search (DFS) to find a path through a maze.
 
     Args:
-      maze_matrix: A 2D list representing the maze.
+      maze: A 2D list representing the maze.
       start: A tuple (row, col) representing the starting position.
       end: A tuple (row, col) representing the ending position.
 
@@ -74,7 +52,7 @@ def dfs(maze_matrix, start, end):
       A list of tuples representing the path found, or None if no path exists.
     """
 
-    rows, cols = len(maze_matrix), len(maze_matrix[0])
+    rows, cols = len(maze), len(maze[0])
     visited = set()
     stack = [(start, [])]
 
@@ -88,10 +66,11 @@ def dfs(maze_matrix, start, end):
 
         for dr, dc in [(0, 1), (1, 0), (0, -1), (-1, 0)]:  # Use the specified order
             new_row, new_col = row + dr, col + dc
-            if 0 <= new_row < rows and 0 <= new_col < cols and (new_row, new_col) not in visited and maze_matrix[new_row][new_col] in ('.', 'E'):
+            if 0 <= new_row < rows and 0 <= new_col < cols and (new_row, new_col) not in visited and maze[new_row][new_col] in ('.', 'E'):
                 stack.append(((new_row, new_col), path + [(row, col)]))
 
     return None
+
 
 #     maze_str = '''.S....
 # ##.#..
@@ -114,17 +93,22 @@ maze_str = '''S#.
 ...
 ..E'''
 
-maze_matrix = maze_to_matrix(maze_str)
-start, end = find_start_end(maze_matrix)
+maze = [[c for c in s] for s in maze_str.strip().split('\n')]
+start, end = find_start_end(maze)
+
+print('Maze')
+for e in maze:
+    print(*e, sep='\t')
+print()
 
 if start is None or end is None:
     print("Start or end position not found in the maze.")
 else:
-    path = dfs(maze_matrix, start, end)
+    path = dfs(maze, start, end)
 
     if path:
         print("Path found:")
         print(path)
-        print_path_in_maze(maze_matrix, path)
+        print_path_in_maze(maze, path)
     else:
         print("No path found.")
